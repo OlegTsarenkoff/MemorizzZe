369777c6488993defa5e40c125a58f2923e243fa
Oleg Tsarenkoff <tsarenkow@icloud.com>
2021-10-13 18:10:31 +0300

    Initial Commit

---
+++ MemorizzZe/MemorizzZeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizzZeApp.swift
+//  MemorizzZe
+//
+//  Created by Oleg Tsarenkoff on 13.10.21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizzZeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
